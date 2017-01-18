e95cde01875bb06a29a9b250c3a77b4c3c6ff5a1
cice <cice@aula1PovMacN09.local>
2017-01-18 11:44:26 +0100

    Colection view/Fotopicker

---
+++ Caras/PersonaCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonaCollectionViewCell.swift
+//  Caras
+//
+//  Created by cice on 18/1/17.
+//  Copyright © 2017 cice. All rights reserved.
+//
+
+import UIKit
+
+class PersonaCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nombre: UILabel!
+    
+    
+}
